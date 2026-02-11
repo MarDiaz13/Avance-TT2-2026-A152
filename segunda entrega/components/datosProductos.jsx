f9c455492110b825d57463024eb0743025bc47c5
Mariana <sofielias132@gmail.com>
2026-02-11 14:40:24 -0600

    Agregando los primero cambios para la siguiente entrega

---
+++ segunda entrega/components/datosProductos.jsx
@@ -0,0 +1,13 @@
+// src/components/datosProductos.js
+export const DATA_PRODUCTOS_INICIAL = [
+  // Productos para Sucursal Poniente
+  { id: '7501055300013', nombre: 'Coca-Cola 600ml', categoria: 'Bebidas', costo: 10.00, precio: 15.00, stock: 48, estado: 'Disponible', sucursal: 'Sucursal Poniente' },
+  { id: '7501055300020', nombre: 'Sabritas Originales 45g', categoria: 'Botanas', costo: 12.00, precio: 18.00, stock: 35, estado: 'Disponible', sucursal: 'Sucursal Poniente' },
+  
+  // Productos para Sucursal Centro
+  { id: '7501055300037', nombre: 'Ruffles Queso 55g', categoria: 'Botanas', costo: 13.00, precio: 20.00, stock: 5, estado: 'Stock Bajo', sucursal: 'Sucursal Centro' },
+  { id: '7501000100019', nombre: 'Bimbo Pan Blanco Grande', categoria: 'Panadería', costo: 28.00, precio: 38.00, stock: 12, estado: 'Disponible', sucursal: 'Sucursal Centro' },
+  
+  // Productos para Sucursal Norte
+  { id: '7501055300044', nombre: 'Leche Lala Entera 1L', categoria: 'Lácteos', costo: 17.00, precio: 22.00, stock: 24, estado: 'Disponible', sucursal: 'Sucursal Norte' },
+];
